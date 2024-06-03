2a90cd65e940b5982fda8a21fca4f9bf2ff83424
babureddyg2308 <158761114+babureddyg2308@users.noreply.github.com>
2024-06-03 18:33:22 +0530

    backend data

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const connectDB = require('./config/db.js');
-const pdfRoutes = require('./routes/pdfRoutes.js');
-const seedDB = require('./seed.js');
+const connectDB = require('./config/db');
+const pdfRoutes = require('./routes/pdfRoutes');
 
 const app = express();
 connectDB();
@@ -12,19 +11,11 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use('/api/pdf', pdfRoutes);
 
-app.get('/', (req, res) => {
+ app.get('/', (req, res) => {
     res.send('Welcome to the PDF generation backend!');
 });
 
-app.get('/seedData', async (req, res) => {
-    try {
-        await seedDB();
-        res.send('Database seeded successfully!');
-    } catch (error) {
-        res.status(500).send(`Error seeding database: ${error.message}`);
-    }
-});
-
-
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
